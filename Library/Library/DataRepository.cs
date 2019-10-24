93c1cd02dcf6217d4bca915f858d752588ba48e4
JKWrobel <bociek428@gmail.com>
2019-10-24 18:06:06 +0200

    Added empty CRUD methods

--- Library/Library/DataRepository.cs
+++ Library/Library/DataRepository.cs
@@ -12,6 +12,85 @@ namespace Library
             _libraryDataBase = new DataContext();
         }
 
+        #region CRUDmethods
+
+        private void GetBookType()
+        {
+
+        }
+
+        private void GetAllTypes()
+        {
+
+        }
+
+        private Guid AddType()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetBookUnit()
+        {
+
+        }
+
+        private void GetAllBookUnits()
+        {
+
+        }
+
+        private Guid AddBookUnit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetUser()
+        {
+
+        }
+
+        private void GetAllUsers()
+        {
+
+        }
+
+        private Guid AddUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetIncident()
+        {
+
+        }
+
+        private void GetAllIncidents()
+        {
+
+        }
+
+        private void AddIncident()
+        {
+
+        }
+
+        #endregion
+
+        #region IncidentStructs
+
+        private interface IIncidentInfo
+        {
+
+        }
+
+        public struct DeliveryInfo : IIncidentInfo
+        {
+
+        }
+
+        #endregion
+
+
         public void AutoFillRepository(IDataFiller dataFiller)
         {
             _dataFiller.Fill(_libraryDataBase);
